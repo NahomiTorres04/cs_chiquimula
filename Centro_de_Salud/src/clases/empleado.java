5fdc907c9580c037dd071c32653d3f733b6dbc0a
alexdlveliz <alexdlveliz@hotmail.com>
2018-06-18 21:14:32 -0600

    Add files via upload

--- Centro_de_Salud/src/clases/empleado.java
+++ Centro_de_Salud/src/clases/empleado.java
@@ -27,13 +27,35 @@ public class empleado
         con = conexion.getConnection();
     }
     
-    public boolean ingresarEmpleado(String nombre, String apellido)
+    public boolean ingresarEmpleado(String nombre, String apellido,String dpi,
+             String municipio, String departamento, String tipo,
+             String clasificacion, String lugar, String cargo, String renglon,
+             String puesto, String fechai, String fechaf,
+             double sueldo, String telefono, String alergias)
     {
         try {
-            String sql = "INSERT into empleado(nombres, apellidos) values (?,?)";
+            String sql = "INSERT into empleado (nombres, apellidos, dpi,"
+                    + " municipio, departamento, tipo, clasificacion,"
+                    + " lugar, cargo, renglon_presupuestario,"
+                    + " puesto, fecha_inicio, fecha_fin,"
+                    + " sueldo, telefono, alergias) values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
             PreparedStatement ps = con.prepareStatement(sql);
             ps.setString(1, nombre);
             ps.setString(2, apellido);
+            ps.setString(3, dpi);
+            ps.setString(4, municipio);
+            ps.setString(5, departamento);
+            ps.setString(6, tipo);
+            ps.setString(7, clasificacion);
+            ps.setString(8, lugar);
+            ps.setString(9, cargo);
+            ps.setString(10, renglon);
+            ps.setString(11, puesto);
+            ps.setString(12, fechai);
+            ps.setString(13, fechaf);
+            ps.setDouble(14, sueldo);
+            ps.setString(15, telefono);
+            ps.setString(16, alergias);
             int n = ps.executeUpdate();
             return n != 0;
         } catch (SQLException ex) {
@@ -41,7 +63,8 @@ public class empleado
         }
         return false;
     }
-    public String getProfesion(String nombre, String apellido)
+    
+    public String getCargo(String nombre, String apellido)
     {
         String profesion = "";
         try {
